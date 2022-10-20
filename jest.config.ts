cfffe357b817d1a48b312789685642cb5907331b
Budakov Dmitry <budakov.it@gmail.com>
2022-10-20 23:44:40 +0300

    Update jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,12 @@ const config: Config = {
   verbose: true,
   testEnvironment: 'jest-environment-jsdom',
   preset: "ts-jest",
+  modulePaths: [
+    "<rootDir>"
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    },
 };
 
 export default config;
