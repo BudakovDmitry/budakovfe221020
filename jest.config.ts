80b50e5de8537f394413b13195dfcc0054047caf
Budakov Dmitry <budakov.it@gmail.com>
2022-10-20 23:23:46 +0300

    Refactor code

--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,6 @@ const config: Config = {
   verbose: true,
   testEnvironment: 'jest-environment-jsdom',
   preset: "ts-jest",
-  transform: {
-    "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest"
-  },
-  transformIgnorePatterns: [
-    "node_modules/(?!variables/.*)"
-  ]
 };
 
 export default config;
